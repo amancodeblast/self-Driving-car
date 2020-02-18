86d42512bfd17fb65fe9f632a9be4a16a33610cd
rockbuzz <sharma.aman7698@gmail.com>
2020-02-18 06:47:48 +0530

    test sense function

---
+++ localization/localization.py
@@ -0,0 +1,41 @@
+#Write code that outputs p after multiplying each entry 
+#by pHit or pMiss at the appropriate places. Remember that
+#the red cells 1 and 2 are hits and the other green cells
+#are misses.#Write code that outputs p after multiplying each entry 
+#by pHit or pMiss at the appropriate places. Remember that
+#the red cells 1 and 2 are hits and the other green cells
+#are misses.
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+def sense(p, Z):
+  q=[]
+  for i in range(len(p)):
+    if world[i]==Z:
+      q.append(p[i]*pHit)
+    else:
+      q.append(p[i]*pMiss)
+  return q
+print(sense(p,Z))
+studentMain.py
+#Question
+#Modify your code so that it normalizes the output for 
+#the function sense. This means that the entries in q 
+#should sum to one.
+
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    return q
+print sense(p,Z)
