1f443d42a74efdd34cf7de795eaa14a0ba038255
rockbuzz <sharma.aman7698@gmail.com>
2020-02-20 07:47:17 +0530

    sense function

--- localization/localization.py
+++ localization/localization.py
@@ -39,3 +39,18 @@ def sense(p, Z):
         q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
     return q
 print sense(p,Z)
+-----------------------------------------------------------------------
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+def sense(p, Z):
+  q=[]
+  for i in range(len(p)):
+    if world[i]==Z:
+      q.append(p[i]*pHit)
+    else:
+      q.append(p[i]*pMiss)
+  return q
+print(sense(p,Z))
