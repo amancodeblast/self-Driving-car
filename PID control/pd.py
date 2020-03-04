69d8c09f9379b44d476172ae0580d3e65705c7e3
rockbuzz <sharma.aman7698@gmail.com>
2020-03-04 07:28:49 +0530

    pd controller solution

--- PID control/pd.py
+++ PID control/pd.py
@@ -121,7 +121,14 @@ def run(robot, tau_p, tau_d, n=100, speed=1.0):
     x_trajectory = []
     y_trajectory = []
     # TODO: your code here
-
+    ctep=robot.y
+    for i in range(n):
+        cte=robot.y
+        steer=-tau_p*cte-(cte-ctep)*tau_d
+        robot.move(steer,speed)
+        x_trajectory.append(robot.x)
+        y_trajectory.append(robot.y)
+        ctep=cte
     return x_trajectory, y_trajectory
     
 x_trajectory, y_trajectory = run(robot, 0.2, 3.0)
