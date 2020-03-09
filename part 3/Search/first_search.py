a4ef8a334c192a5458b3996de7f03116801885f1
rockbuzz <sharma.aman7698@gmail.com>
2020-03-09 09:02:03 +0530

    print path studentMain

--- part 3/Search/first_search.py
+++ part 3/Search/first_search.py
@@ -6,8 +6,7 @@ grid=[[0,0,1,0,0,0],
       [0,0,1,0,0,0],
       [0,0,0,0,1,0],
       [0,0,1,1,1,0],
-      [0,0,0,0,1,0],
-      ]
+      [0,0,0,0,1,0]]
 
 init =[0,0]
 goal=[len(grid)-1,len(grid[0])-1]
@@ -17,3 +16,14 @@ delta=[[-1,0],
        [0,1]]
 dleta_name=["^","<","v",">"]
 cost=1
+g_val=[0,0,0,0]
+for i in range(5):
+    for j in range(6):
+        
+        
+def search():
+    closed=[[0 for row in range(len(grid[0]))]for col in range(len(grid[00]))]
+    closed[init[0]][init[1]]=1
+    x=init[0]
+    y=init[1]
+    gcar
