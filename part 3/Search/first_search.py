b1be1d3d64e6625f2bcd38bbfa2922962d0f8741
rockbuzz <sharma.aman7698@gmail.com>
2020-03-11 22:26:56 +0530

    my final submission

--- part 3/Search/first_search.py
+++ part 3/Search/first_search.py
@@ -27,14 +27,30 @@ def search():
     x=init[0]
     y=init[1]
     open=[[g,x,y]]
-    
-    if open==goal:
-        mila=True
-        for i in range(len(delta_name)):
-            x1=x+delta[i][0]
-            y2=y+delat[i][1]
-            if x1>0 and x1< len(grid[0])and y2 >0 and y2< len(grid):
-                if closed[x1][y1]==0 and grid[x1][y1]==0:
-                    
+    x1=0
+    y1=0
+    while no_escape==False and mila=False:
+        if len(open)==0:
+            no_escape=True
+        else:
+            open.sort()
+            open.reverse()
+            next=open.pop()                
+            for i in range(len(delta_name)):
+                x1=x+delta[i][0]
+                y2=y+delat[i][1]
+                g2=g+next[0]
+                if x1>0 and x1< len(grid[0])and y2 >0 and y2< len(grid):
+                    if closed[x1][y1]==0 and grid[x1][y1]==0:
+                        open.append([g2,x1,y1])
+
+
+
             
+            close[x1][y1]=ArithmeticError
+                        
+    if no_escape==True:
+        print("hamse na ho paega")
+    elif mila==True:
+        print("ni mila")
     
