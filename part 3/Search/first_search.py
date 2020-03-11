fdb297245bb8aec67e5cbd93b4b4611465725c9f
rockbuzz <sharma.aman7698@gmail.com>
2020-03-11 23:00:58 +0530

    the revision for the final submission

--- part 3/Search/first_search.py
+++ part 3/Search/first_search.py
@@ -18,8 +18,8 @@ delta=[[-1,0],
 delta_name=["^","<","v",">"]
 cost=1
 
-no_escape=False
-mila=False
+no_escape=False#flag set if wee cant find expand
+mila=False#flag set if when search is complete
  
 def search():
     closed=[[0 for row in range(len(grid[0]))]for col in range(len(grid))]
@@ -32,25 +32,26 @@ def search():
     while no_escape==False and mila=False:
         if len(open)==0:
             no_escape=True
+            print("Fail")
         else:
             open.sort()
             open.reverse()
-            next=open.pop()                
-            for i in range(len(delta_name)):
-                x1=x+delta[i][0]
-                y2=y+delat[i][1]
-                g2=g+next[0]
-                if x1>0 and x1< len(grid[0])and y2 >0 and y2< len(grid):
-                    if closed[x1][y1]==0 and grid[x1][y1]==0:
-                        open.append([g2,x1,y1])
-
-
-
-            
-            close[x1][y1]=ArithmeticError
-                        
-    if no_escape==True:
-        print("hamse na ho paega")
-    elif mila==True:
-        print("ni mila")
+            next=open.pop()
+            g=next[0]
+            x=next[1]
+            y=next[2]
+            if x=goal[0] and y=goal[1]:
+                mila=True
+                print("milgya")
+            else:
+                for i in range(len(delta_name)):
+                    x1=x+delta[i][0]
+                    y2=y+delat[i][1]
+                    g2=g+next[0]
+                    if x1>=0 and x1< len(grid)and y2 >= 0 and y2 < len(grid[0]):
+                        if closed[x1][y2]==0 and grid[x1][y2]==0:
+                            open.append([g2,x1,y1])
+                            close[x1][y1]=1
+                            
     
+search()
