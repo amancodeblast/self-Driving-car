1050285a7ca04ee75f3794fb0953ace8e1332701
rockbuzz <sharma.aman7698@gmail.com>
2020-03-12 08:13:57 +0530

    syntax error removaal

--- part 3/Search/first_search.py
+++ part 3/Search/first_search.py
@@ -27,13 +27,14 @@ def search():
     closed[init[0]][init[1]]=1
     x=init[0]
     y=init[1]
+    g=0
     open=[[g,x,y]]
     x1=0
     y1=0
-    g=0
+    
     no_escape=False#flag set if wee cant find expand
     mila=False#flag set if when search is complete
-    while no_escape==False and mila=False:
+    while no_escape==False and mila==False:
         if len(open)==0:
             no_escape=True
             print("Fail")
@@ -44,19 +45,20 @@ def search():
             g=next[0]
             x=next[1]
             y=next[2]
-            if x=goal[0] and y=goal[1]:
+            if x==goal[0] and y==goal[1]:
                 mila=True
                 print("milgya")
+                print("[{},{},{}]".format(g,x,y))
             else:
                 for i in range(len(delta_name)):
                     x1=x+delta[i][0]
-                    y2=y+delat[i][1]
+                    y2=y+delta[i][1]
                     
                     if x1>=0 and x1< len(grid)and y2 >= 0 and y2 < len(grid[0]):
                         if closed[x1][y2]==0 and grid[x1][y2]==0:
                             g2=g+cost
-                            open.append([g2,x1,y1])
-                            close[x1][y1]=1
+                            open.append([g2,x1,y2])
+                            closed[x1][y2]=1
                             
     
 search()
