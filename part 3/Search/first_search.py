267721b2cb272e5b6ddca1a909b3c9511bb0e127
rockbuzz <sharma.aman7698@gmail.com>
2020-03-12 07:51:48 +0530

    changing the update of g2 and using cost to update it

--- part 3/Search/first_search.py
+++ part 3/Search/first_search.py
@@ -9,7 +9,7 @@ grid=[[0,0,1,0,0,0],
       [0,0,0,0,1,0]]
 
 init =[0,0]
-g=0
+
 goal=[len(grid)-1,len(grid[0])-1]
 delta=[[-1,0],
        [0,-1],
@@ -18,8 +18,7 @@ delta=[[-1,0],
 delta_name=["^","<","v",">"]
 cost=1
 
-no_escape=False#flag set if wee cant find expand
-mila=False#flag set if when search is complete
+
  
 def search():
     closed=[[0 for row in range(len(grid[0]))]for col in range(len(grid))]
@@ -29,6 +28,9 @@ def search():
     open=[[g,x,y]]
     x1=0
     y1=0
+    g=0
+    no_escape=False#flag set if wee cant find expand
+    mila=False#flag set if when search is complete
     while no_escape==False and mila=False:
         if len(open)==0:
             no_escape=True
@@ -47,9 +49,10 @@ def search():
                 for i in range(len(delta_name)):
                     x1=x+delta[i][0]
                     y2=y+delat[i][1]
-                    g2=g+1
+                    
                     if x1>=0 and x1< len(grid)and y2 >= 0 and y2 < len(grid[0]):
                         if closed[x1][y2]==0 and grid[x1][y2]==0:
+                            g2=g+cost
                             open.append([g2,x1,y1])
                             close[x1][y1]=1
                             
