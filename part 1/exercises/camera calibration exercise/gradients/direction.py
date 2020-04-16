0582227e943cbddd907bc563ae6dfc3f4c9406c2
rockbuzz <sharma.aman7698@gmail.com>
2020-04-17 00:02:00 +0530

    direction gradient solved

--- part 1/exercises/camera calibration exercise/gradients/direction.py
+++ part 1/exercises/camera calibration exercise/gradients/direction.py
@@ -1,4 +1,4 @@
-#QUIZ direction of the gradient
+#QUIZ direction of the gradient(Aman try)
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -12,7 +12,7 @@ image = mpimg.imread('signs_vehicles_xygrad.png')
 # Define a function that applies Sobel x and y, 
 # then computes the direction of the gradient
 # and applies a threshold.
-def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):##point to notice
     
     # Apply the following steps to img
     # 1) Convert to grayscale
@@ -22,6 +22,17 @@ def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
     # 5) Create a binary mask where direction thresholds are met
     # 6) Return this mask as your binary_output image
     binary_output = np.copy(img) # Remove this line
+     # Grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Calculate the x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Take the absolute value of the gradient direction, 
+    # apply a threshold, and create a binary image result
+    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))#point to notice
+    binary_output =  np.zeros_like(absgraddir)
+    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
+
     return binary_output
     
 # Run the function
@@ -35,4 +46,3 @@ ax2.imshow(dir_binary, cmap='gray')
 ax2.set_title('Thresholded Grad. Dir.', fontsize=50)
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 plt.show()
-
