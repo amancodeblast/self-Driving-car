abb302945701edbd8448ccedb4fef562c11fd852
rockbuzz <sharma.aman7698@gmail.com>
2020-05-31 23:47:45 +0530

    search first

--- part 3/Search/first_search.py
+++ part 3/Search/first_search.py
@@ -68,3 +68,63 @@ def search():
     for i in range(len(expand)):
         print(expand[i])
 search()
+
+#### 11. first search best code
+#grid format:
+# 0=navigable space
+# 1=occupied space
+
+grid=[[0,0,1,0,0,0],
+      [0,0,0,0,0,0],
+      [0,0,1,0,1,0],
+      [0,0,1,0,1,0],
+      [0,0,1,0,1,0]]
+# print(' i = ',len(grid))
+# print('i = ,', len(grid[0]))
+
+init =[0,0]
+
+goal=[len(grid)-1,len(grid[0])-1]
+delta=[[-1,0],
+       [0,-1],
+       [1,0],
+       [0,1]]
+delta_name=["^","<","v",">"]
+cost=1
+
+def search():
+    
+    closed = [[0 for i in range(len(grid[0])) ]for j in range(len(grid))]
+    
+    closed[0][0]=1
+    x = init[0]
+    y= init[1]
+    g=0
+    open=[[g,x,y]]
+    found = False
+    quit = False
+    
+    while (found ==False and quit== False):
+        if(len(open))==0:
+            print("fail")
+            quit=True
+        else:
+            open.sort()
+            open.reverse()
+            next=open.pop()
+            
+            x=next[1]
+            y=next[2]
+            g = next[0]
+            if goal[0]==x and goal[1]==y:
+                found=True
+                print('found',next)
+            else:
+                for i in range(len(delta)):
+                    x1= x+ delta[i][0]
+                    y1 = y+ delta[i][1]
+                    if(x1>=0 and x1 < len(grid) and y1>=0 and y1 < len(grid[0])and grid[x1][y1]==0 and closed[x1][y1]==0):                
+                        g1=g+1
+                        open.append([g1,x1,y1])
+                        closed[x1][y1]=1
+search()
